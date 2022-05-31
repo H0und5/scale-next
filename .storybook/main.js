859a7047f2090d76e986aeeaf2633b632104c940
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-31 14:26:31 -0500

    build: implemented storybook, think it works, learned how stories kinda work

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
+
+// staticDirs allows us to use images, fonts and other assets to test our stories.
